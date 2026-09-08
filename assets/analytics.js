a16e3bb3436ec5a3c2bb3d6aeb81692ade2419ac
prahladpkulal <prahlad.pkulal@gmail.com>
2026-09-08 17:37:17 +0530

    feat: connect Google Analytics 4

--- assets/analytics.js
+++ assets/analytics.js
@@ -1,7 +1,27 @@
 (() => {
+  const GA_ID = 'G-D2DS1M4Z34';
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () {
+    window.dataLayer.push(arguments);
+  };
+
+  if (!document.querySelector(`script[src*="googletagmanager.com/gtag/js?id=${GA_ID}"]`)) {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+    document.head.appendChild(script);
+  }
+
+  window.gtag('js', new Date());
+  window.gtag('config', GA_ID, {
+    page_title: document.title,
+    page_location: window.location.href
+  });
+
   const track = (eventName, params = {}) => {
-    window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({ event: eventName, ...params });
+    window.gtag('event', eventName, params);
   };
 
   window.devToolsHub = window.devToolsHub || {};
